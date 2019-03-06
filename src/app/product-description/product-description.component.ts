a42d44c62504eb215ef8b76a06787bc735359fe9
Mark Jeffrey Moreno Lim <Mark_Jeffrey_Lim@manulife.com>
2019-03-06 14:45:12 +0800

    bla

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import 'rxjs/add/operator/map';
-import { Album } from 'app/album';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-description',
